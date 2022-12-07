171aad11e11ee93761f325f8b8b13ceddb66db37
Jsid2022 <vasujain8998@gmail.com>
2022-12-07 21:26:43 +0530

    Added comments

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+// get all elements from html
 const startBtn = document.querySelector('#startGame');
 const gameResultDiv = document.getElementById('gameResult');
 const startDiv = document.querySelector('#startDiv');
@@ -7,17 +8,27 @@ const playerChoice = document.getElementById('playerChoice');
 const computerChoice = document.getElementById('computerChoice');
 const playerScore = document.getElementById('playerScore');
 const computerScore = document.getElementById('computerScore');
-let playerS = playerScore.textContent;
-let computerS = computerScore.textContent;
+
+// Hide game div and result div
 gameDiv.style.display = 'none';
-let gameStarted = false;
-let roundStatus = false;
 gameResultDiv.style.display = "none";
 
+// set both players score to 0
+let playerS = playerScore.textContent;
+let computerS = computerScore.textContent;
 
+// variables necessary to manage game rounds
+let gameStarted = false;
+let roundStatus = false;
+
+
+// on click (start button)
 startBtn.addEventListener('click', () => {
+    // hide start div
     startDiv.style.display = "none";
+    // and display the game div
     gameDiv.style.display = 'flex';
+    // add hover classes to images
     for (let i = 0; i < choicesDiv.length; i++) {
         choicesDiv[i].classList.add('transition');
         choicesDiv[i].classList.add('duration-700');
@@ -25,10 +36,13 @@ startBtn.addEventListener('click', () => {
         choicesDiv[i].classList.add('hover:text-white');
         choicesDiv[i].classList.add('hover:cursor-pointer');
     }
+
+    // set game status to true (game started)
     gameStarted = true;
     roundStatus = true;
 })
 
+// This function will set Player Choice
 const setPlayerChoice = (value) => {
     if (value == 0) {
         playerChoice.src = "images/rock.png";
@@ -39,6 +53,7 @@ const setPlayerChoice = (value) => {
     }
 }
 
+// This function will set computer choice (Randomly)
 const setComputerChoice = () => {
     const randNo = Math.floor(Math.random() * 3);
     if (randNo == 0) {
@@ -51,17 +66,21 @@ const setComputerChoice = () => {
     return randNo;
 }
 
+// This function will reset choices of both player and computer
 const resetChoices = () => {
     playerChoice.src = "images/questionMark.png";
     computerChoice.src = "images/questionMark.png";
     roundStatus = true;
 }
 
+// Start game function
 const startGameRound = (choice) => {
+
     if (gameStarted && roundStatus) {
         setPlayerChoice(choice);
         const randNo = setComputerChoice();
 
+        // Decide the winner of round according to choices
         if (choice != randNo) {
             if (choice === 0 && randNo === 1) {
                 playerScore.textContent = ++playerS;
@@ -77,23 +96,28 @@ const startGameRound = (choice) => {
                 computerScore.textContent = ++computerS;
             }
         }
+
+        // after choice selection pause the game for 1 second
         roundStatus = false;
         setTimeout(resetChoices, 1000);
 
+        // Make changes to DOM if game is over
         if (playerScore.textContent == 3 || computerScore.textContent == 3) {
             if (playerScore.textContent == 3) {
                 gameResultDiv.classList.remove('text-[red]');
                 gameResultDiv.classList.add('text-[green]');
-                gameResultDiv.textContent = "You Won !!";
+                gameResultDiv.textContent = "You Won!!";
             } else {
                 gameResultDiv.classList.add('text-[red]');
                 gameResultDiv.classList.remove('text-[green]');
-                gameResultDiv.textContent = "Computer Won !!";
+                gameResultDiv.textContent = "Computer Won!!";
             }
+            // reset scores
             playerScore.textContent = 0;
             computerScore.textContent = 0;
             playerS = playerScore.textContent;
             computerS = computerScore.textContent;
+            
             gameDiv.style.display = "none";
             startDiv.style.display = "flex";
             startBtn.textContent = "Play Again";
